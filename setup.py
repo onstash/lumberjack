a0d4107e8ca790dfe375fc5dce3be3f38fc30572
Santosh Venkatraman <santosh.venk@gmail.com>
2017-02-24 11:45:53 +0530

    Bumps up version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
     return open(join(dirname(__file__), fname)).read()
 
 setup(name='pylumberjack',
-      version='0.0.1',
+      version='0.0.2',
       description='Python Logging for Humans',
       author='Santosh Venkatraman',
       url='https://github.com/thesantosh/lumberjack',
